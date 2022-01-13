236827851e0a8e2800b63318397c0e91ab9fcf49
victormmorales <victorfasrev@gmail.com>
2022-01-13 11:22:21 +0100

    definir puerto

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express');
 
 //crear servidor
 const app = express();
+
+//Puerto del servidor
+const PORT = process.env.PORT || 4000;
+
+//Iniciar servidor
+app.listen(PORT, () => {
+    console.log(`El servidor está funcionando en el puerto ${PORT}`);
+})
