08c4998ab5c1b7c54a8dfdac9fe3c4ed04b10720
victormmorales <victorfasrev@gmail.com>
2022-01-15 09:28:14 +0100

    obtener todos los proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,6 +1,15 @@
 const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
+const res = require('express/lib/response');
 
 exports.crearProyecto = (req, res) => {
+
+    //revisar si hay errores
+    const errores = validationResult(req);
+    if(!errores.isEmpty()){
+        return res.status(400).json({errores: errores.array()})
+    }
+
     try {
         //Crear proyectos
         const proyecto = new Proyecto(req.body);
@@ -17,3 +26,14 @@ exports.crearProyecto = (req, res) => {
         res.status(500).send('hubo un error');
     }
 };
+
+//Obtener todos los proyectos del usuario
+exports.obtenerProyectos = async (req, res) => {
+    try {
+        const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({ creado: -1});
+        res.json({ proyectos });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
