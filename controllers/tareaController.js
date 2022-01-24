7c61d03b3ba256ce6d5a694182dcc4e099b69500
victormmorales <victorfasrev@gmail.com>
2022-01-24 12:06:50 +0100

    controller tareas

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,6 +1,7 @@
 const Tarea = require("../models/Tarea");
 const Proyecto = require("../models/Proyecto");
 const { validationResult } = require("express-validator");
+const res = require("express/lib/response");
 
 // Crea una nueva tarea
 exports.crearTarea = async (req, res) => {
@@ -8,5 +9,29 @@ exports.crearTarea = async (req, res) => {
   const errores = validationResult(req);
   if (!errores.isEmpty()) {
     return res.status(400).json({ errores: errores.array() });
+  }
+
+  try {
+    //Extraer el proyecto y comprobar si existe
+    const { proyecto } = req.body;
+
+    const existeProyecto = await Proyecto.findById(proyecto);
+    if (!existeProyecto) {
+      return res.status(404).json({ msg: "Proyecto no encontrado" });
+    }
+
+    //revisar si el proyecto pertenece al usuario
+    if (existeProyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "No autorizado" });
+    }
+
+    //crear tarea
+    const tarea = new Tarea(req.body);
+    await tarea.save();
+
+    res.json({ tarea });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
   }
 };
